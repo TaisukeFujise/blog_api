5a0f08535d7b1738188266651ecdfeb8719d978d
TaisukeFujise <tafujise3502@icloud.com>
2026-04-16 17:26:06 +0900

    add handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TaisukeFujise/blog_api
 
 go 1.25.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/TaisukeFujise/blog_api/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
