8a99276611b0c1c26f9f1d082b74d5ba0810aa2a
TaisukeFujise <tafujise3502@icloud.com>
2026-04-22 15:37:57 +0900

    feat: add channel for GetArticleService

--- services/article_service.go
+++ services/article_service.go
@@ -3,7 +3,6 @@ package services
 import (
 	"database/sql"
 	"errors"
-	"sync"
 
 	"github.com/TaisukeFujise/blog_api/apperrors"
 	"github.com/TaisukeFujise/blog_api/models"
@@ -15,41 +14,36 @@ func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) 
 	var commentList []models.Comment
 	var articleGetErr, commentGetErr error
 
-	// Lock周り
-	var amu sync.Mutex
-	var cmu sync.Mutex
-	var wg sync.WaitGroup
-	wg.Add(2)
+	type articleResult struct {
+		article models.Article
+		err     error
+	}
+	articleChan := make(chan articleResult)
+	defer close(articleChan)
+	go func(ch chan<- articleResult, db *sql.DB, articleID int) {
+		article, err := repositories.SelectArticleDetail(db, articleID)
+		ch <- articleResult{article: article, err: err}
+	}(articleChan, s.db, articleID)
 
-	go func(db *sql.DB, articleID int) {
-		defer wg.Done()
-		amu.Lock()
-		article, articleGetErr = repositories.SelectArticleDetail(db, articleID)
-		amu.Unlock()
-	}(s.db, articleID)
-	// article, err := repositories.SelectArticleDetail(s.db, articleID)
-	// if err != nil {
-	// 	if errors.Is(err, sql.ErrNoRows) {
-	// 		err = apperrors.NAData.Wrap(err, "no data")
-	// 		return models.Article{}, err
-	// 	}
-	// 	err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
-	// 	return models.Article{}, err
-	// }
+	type commentResult struct {
+		commentList *[]models.Comment
+		err         error
+	}
+	commentChan := make(chan commentResult)
+	defer close(commentChan)
+	go func(ch chan<- commentResult, db *sql.DB, articleID int) {
+		commentList, err := repositories.SelectCommentList(db, articleID)
+		ch <- commentResult{commentList: &commentList, err: err}
+	}(commentChan, s.db, articleID)
 
-	go func(db *sql.DB, articleID int) {
-		defer wg.Done()
-		cmu.Lock()
-		commentList, commentGetErr = repositories.SelectCommentList(db, articleID)
-		cmu.Unlock()
-	}(s.db, articleID)
-	// commentList, err := repositories.SelectCommentList(s.db, articleID)
-	// if err != nil {
-	// 	err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
-	// 	return models.Article{}, err
-	// }
-
-	wg.Wait()
+	for i := 0; i < 2; i++ {
+		select {
+		case ar := <-articleChan:
+			article, articleGetErr = ar.article, ar.err
+		case cr := <-commentChan:
+			commentList, commentGetErr = *cr.commentList, cr.err
+		}
+	}
 
 	if articleGetErr != nil {
 		if errors.Is(articleGetErr, sql.ErrNoRows) {
